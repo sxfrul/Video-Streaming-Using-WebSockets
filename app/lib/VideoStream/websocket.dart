514ea19e01d4e14f58779c3853e7d435b6c88819
sxfrul <muhdsafrul@gmail.com>
2024-01-01 14:15:27 +0800

    + dart files ip addressed to raspi

--- app/lib/VideoStream/websocket.dart
+++ app/lib/VideoStream/websocket.dart
@@ -33,7 +33,7 @@ class WebSocket {
 
   /// Connects the current application to a websocket
   void connect() async {
-    _channel = WebSocketChannel.connect(Uri.parse("ws://192.168.0.140:80"));
+    _channel = WebSocketChannel.connect(Uri.parse("ws://192.168.0.170:80"));
   }
 
   /// Disconnects the current application from a websocket
--- app/lib/constants/constants.dart
+++ app/lib/constants/constants.dart
@@ -1,3 +1,3 @@
 class Constants {
-  static String videoWebsocketURL = "ws://192.168.0.140:80";
+  static String videoWebsocketURL = "ws://192.168.0.170:80";
 }
